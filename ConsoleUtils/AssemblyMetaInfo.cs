a547746d9e238c736faa9982edd1fd4f96d006e2
Prasanna V. Loganathar <pvl@prasannavl.com>
2014-04-03 23:30:03 +0530

    Add initial files

---
+++ ConsoleUtils/AssemblyMetaInfo.cs
@@ -0,0 +1,128 @@
+﻿// Author: Prasanna V. Loganathar
+// Project: ConsoleUtils
+// Copyright (c) Launchark. All rights reserved.
+// See License.txt in the project root for license information.
+//  
+// Created: 9:24 PM 03-04-2014
+
+namespace ConsoleUtils
+{
+    using System;
+    using System.Collections.Specialized;
+    using System.Reflection;
+
+    public class AssemblyMetaInfo
+    {
+        private const int DescriptionBit = 1 << 0;
+        private const int CompanyBit = 1 << 1;
+        private const int TitleBit = 1 << 2;
+        private const int CopyrightBit = 1 << 3;
+
+        private string company;
+        private string copyright;
+        private string description;
+        private string title;
+
+        private BitVector32 initStatus = new BitVector32(0);
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="T:System.Object" /> class.
+        /// </summary>
+        public AssemblyMetaInfo()
+        {
+            Assembly = Assembly.GetExecutingAssembly();
+        }
+
+        public AssemblyMetaInfo(Assembly assembly)
+        {
+            if (assembly == null)
+            {
+                throw new ArgumentNullException("assembly");
+            }
+
+            Assembly = assembly;
+        }
+
+        public string Description
+        {
+            get
+            {
+                if (!initStatus[DescriptionBit])
+                {
+                    var descAttr = Assembly.GetCustomAttribute<AssemblyDescriptionAttribute>();
+                    Description = descAttr == null ? string.Empty : descAttr.Description;
+                }
+                return description;
+            }
+            set
+            {
+                description = value;
+                initStatus[DescriptionBit] = true;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                if (!initStatus[TitleBit])
+                {
+                    var titleAttr = Assembly.GetCustomAttribute<AssemblyTitleAttribute>();
+                    Title = titleAttr == null ? string.Empty : titleAttr.Title;
+                }
+                return title;
+            }
+            set
+            {
+                title = value;
+                initStatus[TitleBit] = true;
+            }
+        }
+
+        public string Copyright
+        {
+            get
+            {
+                if (!initStatus[CopyrightBit])
+                {
+                    var copyrightAttr = Assembly.GetCustomAttribute<AssemblyCopyrightAttribute>();
+                    Copyright = copyrightAttr == null ? string.Empty : copyrightAttr.Copyright;
+                }
+                return copyright;
+            }
+            set
+            {
+                copyright = value;
+                initStatus[CopyrightBit] = true;
+            }
+        }
+
+        public virtual string CopyrightNormalizedText
+        {
+            get
+            {
+                return Copyright.Replace("© ", string.Empty);
+            }
+        }
+
+        public string Company
+        {
+            get
+            {
+                if (!initStatus[CompanyBit])
+                {
+                    var companyAttr = Assembly.GetCustomAttribute<AssemblyCompanyAttribute>();
+                    Company = companyAttr == null ? string.Empty : companyAttr.Company;
+                }
+                return company;
+            }
+            set
+            {
+                company = value;
+                initStatus[CompanyBit] = true;
+            }
+        }
+
+        public Assembly Assembly { get; private set; }
+    }
+}
---
+++ ConsoleUtils/ExtendedConsole.cs
@@ -0,0 +1,198 @@
+﻿// Author: Prasanna V. Loganathar
+// Project: ConsoleUtils
+// Copyright (c) Launchark. All rights reserved.
+// See License.txt in the project root for license information.
+//  
+// Created: 9:21 PM 03-04-2014
+
+namespace ConsoleUtils
+{
+    using System;
+
+    public static class ExtendedConsole
+    {
+        public static ConsoleColor ErrorColor = ConsoleColor.Red;
+        public static ConsoleColor WarningColor = ConsoleColor.DarkYellow;
+        public static ConsoleColor SuccessColor = ConsoleColor.Green;
+        public static ConsoleColor InfoColor = ConsoleColor.DarkGray;
+
+        public static void ColoredAction(ConsoleColor textColor, Action action)
+        {
+            ColoredAction(textColor, Console.BackgroundColor, action);
+        }
+
+        public static void ColoredAction(ConsoleColor textColor, ConsoleColor backgroundColor, Action action)
+        {
+            var currentFgColor = Console.ForegroundColor;
+            var currentBgColor = Console.BackgroundColor;
+            Console.ForegroundColor = textColor;
+            Console.BackgroundColor = backgroundColor;
+            action();
+            Console.ForegroundColor = currentFgColor;
+            Console.BackgroundColor = currentBgColor;
+        }
+
+        public static void WriteErrorLine(object value)
+        {
+            WriteLine(ErrorColor, value);
+        }
+
+        public static void WriteErrorLine(string format, params object[] args)
+        {
+            WriteLine(ErrorColor, format, args);
+        }
+
+        public static void WriteError(object value)
+        {
+            Write(ErrorColor, value);
+        }
+
+        public static void WriteError(string format, params object[] args)
+        {
+            Write(ErrorColor, format, args);
+        }
+
+        public static void WriteInfoLine(object value)
+        {
+            WriteLine(InfoColor, value);
+        }
+
+        public static void WriteInfoLine(string format, params object[] args)
+        {
+            WriteLine(InfoColor, format, args);
+        }
+
+        public static void WriteInfo(object value)
+        {
+            Write(InfoColor, value);
+        }
+
+        public static void WriteInfo(string format, params object[] args)
+        {
+            Write(InfoColor, format, args);
+        }
+
+        public static void WriteWarningLine(object value)
+        {
+            WriteLine(WarningColor, value);
+        }
+
+        public static void WriteWarningLine(string format, params object[] args)
+        {
+            WriteLine(WarningColor, format, args);
+        }
+
+        public static void WriteWarning(object value)
+        {
+            Write(WarningColor, value);
+        }
+
+        public static void WriteWarning(string format, params object[] args)
+        {
+            Write(WarningColor, format, args);
+        }
+
+        public static void WriteSuccessLine(object value)
+        {
+            WriteLine(SuccessColor, value);
+        }
+
+        public static void WriteSuccessLine(string format, params object[] args)
+        {
+            WriteLine(SuccessColor, format, args);
+        }
+
+        public static void WriteSuccess(object value)
+        {
+            Write(SuccessColor, value);
+        }
+
+        public static void WriteSuccess(string format, params object[] args)
+        {
+            Write(SuccessColor, format, args);
+        }
+
+        public static void WriteLine(ConsoleColor textColor, object value)
+        {
+            ColoredAction(textColor, () => Console.WriteLine(value));
+        }
+
+        public static void Write(ConsoleColor textColor, string format, params object[] args)
+        {
+            ColoredAction(textColor, () => Console.Write(format, args));
+        }
+
+        public static void Write(ConsoleColor textColor, object value)
+        {
+            ColoredAction(textColor, () => Console.Write(value));
+        }
+
+        public static void WriteLine(ConsoleColor textColor, string format, params object[] args)
+        {
+            ColoredAction(textColor, () => Console.WriteLine(format, args));
+        }
+
+        public static void WriteCentered(
+            string text,
+            char fillerChar = ' ',
+            int leftSpacing = 1,
+            int rightSpacing = 1,
+            bool ignoreRight = false)
+        {
+            var innerLength = text.Length + leftSpacing + rightSpacing;
+            var fillerLength = (Console.WindowWidth - innerLength) / 2;
+            Console.Write(new string(fillerChar, fillerLength));
+            Console.Write(new string(' ', leftSpacing) + text + new string(' ', rightSpacing));
+
+            // Remove an extra filler to make sure the content is centralized regardless of even or odd length.
+            if (!ignoreRight)
+            {
+                Console.Write(new string(fillerChar, innerLength % 2 == 0 ? fillerLength - 1 : fillerLength));
+            }
+        }
+
+        public static void WriteLineCentered(
+            string text,
+            char fillerChar = ' ',
+            int leftSpacing = 1,
+            int rightSpacing = 1)
+        {
+            WriteCentered(text, fillerChar, leftSpacing, rightSpacing);
+            Console.WriteLine();
+        }
+
+        public static void FillRow(char c)
+        {
+            Console.Write(new string(c, Console.WindowWidth - 1));
+        }
+
+        public static void FillLine(char c)
+        {
+            FillRow(c);
+            Console.WriteLine();
+        }
+
+        public static void FillRemainingLine(char c)
+        {
+            FillRemainingRow(c);
+            Console.WriteLine();
+        }
+
+        public static void FillRemainingRow(char c)
+        {
+            Console.Write(new string(c, Console.WindowWidth - Console.CursorLeft - 1));
+        }
+
+        public static void RepeatString(string text, int numberOfTimes, bool breakLines = false)
+        {
+            for (int i = 0; i < numberOfTimes; i++)
+            {
+                Console.Write(text);
+                if (breakLines)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
